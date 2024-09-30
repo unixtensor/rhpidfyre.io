59ff1d9b4b9b60371019a6a8092d5da1eb9b6d32
Kamp <brandon@rhpidfyre.io>
2024-09-30 16:12:57 -0400

    vue integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
+	integrations: [vue()],
 	vite: {
 		css: {
 			preprocessorOptions: {
---
+++ src/components/vue/tsparticles.vue
@@ -0,0 +1,96 @@
+<script lang="ts">
+	import {createApp} from "vue";
+	import {loadSlim} from "@tsparticles/slim";
+	import Particles from "@tsparticles/vue3";
+
+	createApp({}).use(Particles, {
+		init: async engine => {
+			// await loadFull(engine); // you can load the full tsParticles library from "tsparticles" if you need it
+			await loadSlim(engine); // or you can load the slim version from "@tsparticles/slim" if don't need Shapes or Animations
+		},
+	});
+</script>
+
+<template>
+	<div id="app">
+		<vue-particles
+            id="tsparticles"
+            @particles-loaded="particlesLoaded"
+            :options="{
+				background: {
+					color: {
+						value: '#0d47a1'
+					}
+				},
+				fpsLimit: 120,
+				interactivity: {
+					events: {
+						onClick: {
+							enable: true,
+							mode: 'push'
+						},
+						onHover: {
+							enable: true,
+							mode: 'repulse'
+						},
+					},
+					modes: {
+						bubble: {
+							distance: 400,
+							duration: 2,
+							opacity: 0.8,
+							size: 40
+						},
+						push: {
+							quantity: 4
+						},
+						repulse: {
+							distance: 200,
+							duration: 0.4
+						}
+					}
+				},
+				particles: {
+					color: {
+						value: '#ffffff'
+					},
+					links: {
+						color: '#ffffff',
+						distance: 150,
+						enable: true,
+						opacity: 0.5,
+						width: 1
+					},
+					move: {
+						direction: 'none',
+						enable: true,
+						outModes: 'bounce',
+						random: false,
+						speed: 6,
+						straight: false
+					},
+					number: {
+						density: {
+							enable: true,
+						},
+						value: 80
+					},
+					opacity: {
+						value: 0.5
+					},
+					shape: {
+						type: 'circle'
+					},
+					size: {
+						value: { min: 1, max: 5 }
+					}
+				},
+				detectRetina: true
+			}"
+        />
+	</div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
