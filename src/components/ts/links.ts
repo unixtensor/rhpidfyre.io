ca83b6cdd8d89b5c48b5d4840c42c6fa999fd174
Kamp <brandon@rhpidfyre.io>
2024-10-01 16:28:53 -0400

    vue testing

---
+++ src/components/ts/links.ts
@@ -0,0 +1,9 @@
+export const enum httpsLinks {
+	AstroLink = "https://astro.build/",
+	SourceCodeLink = "https://git.rhpidfyre.io/rhpidfyre/rhpidfyre.io",
+	CodeRepositoriesLink = "https://git.rhpidfyre.io/",
+	GitAvatar = "https://git.rhpidfyre.io/avatars/0c7d716d6c8dccee5be5b8b7fe8be8ff",
+	GitProfile = "https://git.rhpidfyre.io/rhpidfyre",
+	ServersLink = "",
+	ContactLink = "",
+}
---
+++ src/components/vue/tsparticles/app.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+	import {createApp} from 'vue';
+	import {loadSlim} from "tsparticles-slim";
+	import {Config as ParticlesConfig} from './config';
+	import Particles from "@tsparticles/vue3";
+	
+	createApp({}).use(Particles, {
+		init: async engine => await loadSlim(engine as any) //please fix your api types
+	})
+</script>
+
+<template>
+	<div id="app">
+		<Particles id="tsparticles" :options="ParticlesConfig"/>
+	</div>
+</template>
+
+<style scoped>
+	#app {
+		width: inherit;
+		height: inherit;
+		user-select: none;
+		pointer-events: none;
+		position: absolute;
+	}
+</style>
