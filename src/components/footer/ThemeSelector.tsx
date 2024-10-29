fe60be70a397b2dbca86781c1adf28e6507fae50
unixtensor <brandon@rhpidfyre.io>
2024-10-29 13:59:49 -0400

    merge

--- src/components/footer/ThemeSelector.tsx
+++ src/components/footer/ThemeSelector.tsx
@@ -1,26 +1,23 @@
 import {Themes} from "../ts/enums"
+import type {SvgProps} from "../ts/types"
 import "../../scss/react/footer/button.scss"
 
-const ChangeTheme = (Theme: Themes) => {
+const OnClick = (Theme: Themes) => {
 	const CssRoot = document.querySelector(":root")
 	if (CssRoot) {
-		CssRoot.setAttribute("theme", Theme.toString())
+		CssRoot.setAttribute("data-theme", Theme.toString())
 	} else {
 		console.warn(`:root returned null, got: ${CssRoot}`)
 	}
 }
 
-type SvgDefaultSize = "20"
-type SvgSize<T extends string = SvgDefaultSize> = `${T}px` | `${SvgDefaultSize}px`
-interface AstroProps {
-	Icon: Themes,
-	Height: SvgSize,
-	Width: SvgSize
+interface AstroProps extends SvgProps {
+	Icon: Themes
 }
-const SelectTheme = (AstroProps: AstroProps) => {
+const ThemeSelector = (AstroProps: AstroProps) => {
 	return (<>
-		<button onClick={() => ChangeTheme(AstroProps.Icon)}>
-			<svg xmlns="http://www.w3.org/2000/svg" height={AstroProps.Height || "20px"} width={AstroProps.Width || "20px"} viewBox="0 -960 960 960">
+		<button onClick={() => OnClick(AstroProps.Icon)}>
+			<svg height={AstroProps.Height || "20px"} width={AstroProps.Width || "20px"} fill={AstroProps.Color || "#e8eaed"} viewBox="0 -960 960 960">
 				{AstroProps.Icon === Themes.Dark &&
 					<path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z"/>
 				||
@@ -31,4 +28,4 @@ const SelectTheme = (AstroProps: AstroProps) => {
 	</>)
 }
 
-export default SelectTheme
+export default ThemeSelector
