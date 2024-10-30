3b0bc0d9bf9ad9a1fc614aca9f5a9fdb8e23c63c
unixtensor <brandon@rhpidfyre.io>
2024-10-29 21:25:10 -0400

    logo effect

--- src/components/ts/enums.ts
+++ src/components/ts/enums.ts
@@ -4,17 +4,19 @@ export const enum Themes {
 }
 
 export const enum HttpsLinks {
+	HomeLink = "https://rhpidfyre.io/",
 	AstroLink = "https://astro.build/",
 	SourceCodeLink = "https://git.rhpidfyre.io/rhpidfyre/rhpidfyre.io",
 	CodeRepositoriesLink = "https://git.rhpidfyre.io/",
 	GitAvatar = "https://git.rhpidfyre.io/avatars/bc887366f12eb8711c8783f4cd473d4169ac8058230e04cd8c51c3a2d98c6d04",
 	GitProfile = "https://git.rhpidfyre.io/rhpidfyre",
 	ServersLink = "",
-	ContactLink = "",
+	CloudLink = "https://content.rhpidfyre.io/",
 }
 
 export const enum SvgIcons {
 	DNS,
 	Home,
-	Repositories
+	Repositories,
+	Cloud
 }
