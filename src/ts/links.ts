f5f335ff3364428200e14dd540d892d7bfb655b3
rhpidfyre <brandon@rhpidfyre.io>
2025-02-02 00:58:28 -0500

    add react

---
+++ src/ts/links.ts
@@ -0,0 +1,10 @@
+const enum Links {
+	Self = "https://rhpidfyre.io/",
+	Cloud = "https://files.rhpidfyre.io/",
+	Gsm = "https://gsm.rhpidfyre.io/",
+	Git = "https://git.rhpidfyre.io/",
+	RepoGitea = "https://git.rhpidfyre.io/rhpidfyre/rhpidfyre.io/",
+	RepoGithub = "https://github.com/unixtensor/rhpidfyre.io/"
+}
+
+export { Links }
